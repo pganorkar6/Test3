b06e8fd044ae7eee13649e38dd2eaae701916146
pganorkat6 <pganorkar6@gmail.com>
2023-07-08 14:23:39 +0530

    [Pratik]I have created Test3

---
+++ src/trial/ClassA.java
@@ -0,0 +1,9 @@
+package trial;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		System.out.println("Hi I have created class A");
+	}
+
+}
---
+++ src/trial/ClassB.java
@@ -0,0 +1,9 @@
+package trial;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		System.out.println("Hi I have created class B");
+	}
+
+}
